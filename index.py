d2cce35266bef3efa38fa899af27c2cee7407c3b
damunguiae <david@munguiae.com>
2023-01-16 16:02:53 -0600

    bug added lol

--- index.py
+++ index.py
@@ -2,8 +2,9 @@ import numpy as np
 
 test_list = [1,2,3,4,5]
 test_matrix = [test_list,test_list,test_list,test_list,test_list]
-
+print(test_matrix)
+# a matrix is like (elements, properties of elements) transposed - > (properties, all data for that property, for all elements)
 matrix_np= np.array(test_matrix)
 
 print(matrix_np.shape)
-
+print(matrix_np)
